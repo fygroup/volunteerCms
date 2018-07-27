4fc992508d05954e0f76f235931e4492c5e9d5dd
MyNameIsPG <493871850@qq.com>
2018-07-27 15:03:58 +0800

    first commit

---
+++ src/components/common/sidebar.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="__sidebar">
+        <el-row class="tac">
+            <el-col>
+                <el-menu
+                class="el-menu-vertical-demo"
+                :default-openeds="openeds"
+                @open="handleOpen"
+                @close="handleClose"
+                :unique-opened="uniqueOpened"
+                :router="openedRouter"
+                :default-active="defaultActive"
+                background-color="#20222a"
+                text-color="#fff"
+                active-text-color="#fff">
+                    <el-submenu v-for="(item,index) in sidebarMenu" :key="index" :index="''+(index+1)">
+                        <template slot="title">
+                            <i :class="item.parentItem.icon"></i>
+                            <span>{{item.parentItem.name}}</span>
+                        </template>
+                        <el-menu-item-group v-if="item.childItem">
+                            <el-menu-item v-for="(itemList,indexList) in item.childItem" :key="indexList" :index="''+itemList.path">
+                                {{itemList.name}}
+                            </el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            uniqueOpened: true,
+            openedRouter: true,
+            defaultActive: this.$route.path,
+            openeds: ['1'],
+            sidebarMenu: [
+                {
+                    parentItem: { name: '内容管理', icon: 'el-icon-location' },
+                    childItem: [
+                        { name: '文章管理', path: '/home/contern/article' },
+                        { name: '视频管理', path: '/home/contern/article' },
+                        { name: '栏目管理', path: '/home/contern/column' },
+                        { name: '活动管理', path: '/home/contern/activity' },
+                        { name: '任务管理', path: '/home/contern/task' },
+                        { name: '公告管理', path: '/home/contern/notice' },
+                    ],
+                },
+                {
+                    parentItem: { name: '社区管理', icon: 'el-icon-location' },
+                    childItem: [
+                        { name: '社区管理', path: '/home/community/organization' },
+                        { name: '居民管理', path: '/home/community/personnel' },
+                    ],
+                },
+                {
+                    parentItem: { name: '商品管理', icon: 'el-icon-location' },
+                    childItem: [
+                        { name: '商品管理', path: '/home/goods/goodsList' },
+                    ],
+                },
+                {
+                    parentItem: { name: '权限管理', icon: 'el-icon-location' },
+                    childItem: [
+                        { name: '管理员', path: '/home/power/powerUser' },
+                        { name: '权限组', path: '/home/power/powerPowerGroup' },
+                    ],
+                }
+            ]
+        }
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+           	
+        },
+        handleClose(key, keyPath) {
+            
+        },
+    },
+}
+</script>
+
+<style lang="stylus">
+.sidebar {
+    .el-menu-item-group__title {
+        padding 0px;
+    }
+    .el-submenu .el-menu-item {
+        background #16181d !important
+    }
+    .el-submenu .is-active {
+        background #1E9FFF !important
+    }
+    
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router);
+
+import login from '@/components/login';
+import wrapper from '@/components/wrapper';
+import survey from '@/components/survey';//首页统计
+/*内容管理*/
+import article from '@/components/totalManag/content/article';//文章管理
+import articleAdd from '@/components/totalManag/content/articleAdd';//新增文章
+import column from '@/components/totalManag/content/column';//栏目管理
+import columnAdd from '@/components/totalManag/content/columnAdd';//新增栏目
+import activity from '@/components/totalManag/content/activity';//活动管理
+import activityAdd from '@/components/totalManag/content/activityAdd';//新增活动
+import task from '@/components/totalManag/content/task';//任务管理
+import taskAdd from '@/components/totalManag/content/taskAdd';//新增任务
+import notice from '@/components/totalManag/content/notice';//公告管理
+import noticeAdd from '@/components/totalManag/content/noticeAdd';//新增公告
+/*社区管理*/
+import organization from '@/components/totalManag/community/organization';//社区管理
+import organizationAdd from '@/components/totalManag/community/organizationAdd';//新增社区
+import personnel from '@/components/totalManag/community/personnel';//居民列表
+/*商品管理*/
+import goodsList from '@/components/totalManag/goods/goodsList';//商品列表
+import goodsListAdd from '@/components/totalManag/goods/goodsListAdd';//新增列表
+/*权限管理*/
+import powerUser from '@/components/totalManag/power/user';//管理员
+import powerPowerGroup from '@/components/totalManag/power/powerGroup';//权限组
+
+export default new Router({
+	routes: [
+		{
+			path: '/login',
+			name: '登录',
+			component: login,
+		},
+		{
+			path: '/home',
+			name: '主页',
+			component: wrapper,
+			children: [
+				{ path: '/home', redirect: 'survey' },
+				{ path: '/home/survey', name: '概况', component: survey },
+				//内容模块
+				{ path: '/home/contern/article', name: '文章管理', component: article },
+				{ path: '/home/contern/articleAdd', name: '新增文章', component: articleAdd },
+				{ path: '/home/contern/column', name: '栏目管理', component: column },
+				{ path: '/home/contern/columnAdd', name: '新增栏目', component: columnAdd },
+				{ path: '/home/contern/activity', name: '活动管理', component: activity },
+				{ path: '/home/contern/activityAdd', name: '新增活动', component: activityAdd },
+				{ path: '/home/contern/task', name: '任务管理', component: task },
+				{ path: '/home/contern/taskAdd', name: '新增任务', component: taskAdd },
+				{ path: '/home/contern/notice', name: '公告管理', component: notice },
+				{ path: '/home/contern/noticeAdd', name: '新增管理', component: noticeAdd },
+				//社区管理
+				{ path: '/home/community/organization', name: '社区管理', component: organization },
+				{ path: '/home/community/organizationAdd', name: '新增社区', component: organizationAdd },
+				{ path: '/home/community/personnel', name: '居民列表', component: personnel },
+				//商品管理
+				{ path: '/home/goods/goodsList', name: '商品管理', component: goodsList },
+				{ path: '/home/goods/goodsListAdd', name: '新增商品', component: goodsListAdd },
+				//权限管理
+				{ path: '/home/power/powerUser', name: '管理员', component: powerUser },
+				{ path: '/home/power/powerPowerGroup', name: '权限组', component: powerPowerGroup },
+            ]
+		}
+	]
+})
